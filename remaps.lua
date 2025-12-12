7f4e5ed4d8c74ecf3d32339292abe0d19f15f5ee
Arjun Gore <arjun.c.gore@gmail.com>
2025-12-12 05:00:09 -0500

    Initial Commit: Need to move some remaps to xkb instead, and change search crafts to new one I thought of of wtv idrc hi xv

---
+++ remaps.lua
@@ -0,0 +1,41 @@
+local remaps = {
+    enabled = {
+        ["MB4"] = "F3",
+        ["LEFTALT"] = "LEFTCTRL",
+
+        ["MB5"] = "Home",
+
+        ["O"] = "Q",
+        ["Q"] = "O",
+        ["T"] = "BACKSPACE",
+        ["BACKSPACE"] = "T",
+        ["F1"] = "I",
+        ["I"] = "F1",
+        ["D"] = "X",
+        ["X"] = "RIGHTSHIFT",
+        ["RIGHTSHIFT"] = "D",
+        ["A"] = "K",
+        ["Z"] = "A",
+        ["CAPSLOCK"] = "0",
+
+        ["K"] = "CAPSLOCK",
+        ["0"] = "Z",
+
+        -- ["MB5"] = "Home",
+        -- ["Q"] = "O",
+        -- ["A"] = "N",
+        -- ["D"] = "X" ,
+        -- ["CAPSLOCK"] = "0",
+        -- ["X"] = "RIGHTSHIFT",
+        -- ["O"] = "Q",
+    },
+
+    disabled = {
+        ["MB4"] = "F3",
+        ["MB5"] = "F6",
+        ["F6"] = "MB5",
+        ["LEFTALT"] = "LEFTCTRL",
+    }
+}
+
+return remaps
