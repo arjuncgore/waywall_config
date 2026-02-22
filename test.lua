a6db4f985d0fb2e4f3f7769e843432f582c42f43
Arjun Gore <arjun.c.gore@gmail.com>
2026-02-22 01:37:36 -0500

    testing file

---
+++ test.lua
@@ -0,0 +1,12 @@
+local waywall = require("waywall")
+local helpers = require("waywall.helpers")
+
+local M = {}
+
+M.send = function(config)
+    -- ========================== BEGIN TEST
+
+    -- ========================== END TEST
+end
+
+return M
